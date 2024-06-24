39a8b60b60cc1b5e7541fd2f7afb18cd57a28d00
Nhờ <huunhodc4869@gmail.com>
2024-06-24 19:55:25 +0700

    Thêm các controller, views và sửa độ dài DocId bằng 35 và PostTags

---
+++ DocumentSharing/DocumentSharing/Controllers/RoleController.cs
@@ -0,0 +1,16 @@
+﻿using DocumentSharing.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DocumentSharing.Controllers {
+    public class RoleController : Controller {
+        private readonly DocumentSharingContext _context;
+
+        public RoleController(DocumentSharingContext context) {
+            _context = context;
+        }
+
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
