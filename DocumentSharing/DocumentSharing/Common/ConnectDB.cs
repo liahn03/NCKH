c34e4f1029a5babdd8ef86984256f8b0490fc3c2
TienLam2003 <tienlamnguyen2003@gmail.com>
2024-06-25 10:50:12 +0700

    update code

---
+++ DocumentSharing/DocumentSharing/Common/ConnectDB.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace DocumentSharing.Common
+{
+    public class ConnectDB
+    {
+        public static SqlConnection conMyConnection;
+        public static void ConnectDatabase()
+        {
+            string strConn = @"Data Source=TIENLAM\SQLEXPRESS;Initial Catalog=DocumentSharing;Integrated Security=True;TrustServerCertificate=True;";
+            conMyConnection = new SqlConnection();
+            conMyConnection.ConnectionString = strConn;
+            conMyConnection.Open();
+        }
+    }
+}
---
+++ DocumentSharing/DocumentSharing/DAL/DocumentDAO.cs
@@ -0,0 +1,22 @@
+﻿using DocumentSharing.Common;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace DocumentSharing.DAL
+{
+    public class DocumentDAO
+    {
+        public DataTable GetDocument()
+        {
+            ConnectDB.ConnectDatabase();
+            SqlConnection conn = ConnectDB.conMyConnection;
+            string query = @"SELECT RoleId AS RoleId, RoleName AS RoleName FROM AppRole";
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, conn);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+
+            return dt;
+
+        }
+    }
+}
