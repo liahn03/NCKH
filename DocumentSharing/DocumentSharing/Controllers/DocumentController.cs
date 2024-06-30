ba0bf915c71504eb2c1683595f8e8b66eb460a25
TienLam2003 <tienlamnguyen2003@gmail.com>
2024-06-30 19:41:10 +0700

    Thêm phân trang dữ liệu

--- DocumentSharing/DocumentSharing/Controllers/DocumentController.cs
+++ DocumentSharing/DocumentSharing/Controllers/DocumentController.cs
@@ -1,24 +1,75 @@
-
-﻿using DocumentSharing.DAL;
+using DocumentSharing.DAL;
 using DocumentSharing.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Data;
+using System.Runtime.CompilerServices;
 
 namespace DocumentSharing.Controllers
-{    
+{
     public class DocumentController : Controller
     {
         DocumentDAO docDAO = new DocumentDAO();
         public IActionResult Index()
         {
-            DataTable dt = docDAO.GetDocument();
+            return View();
+        }
+
+        public IActionResult DocumentList(int pageNumber = 1)
+        {
+            DataTable dt = docDAO.GetDocument(pageNumber);
             List<Models.Document> listDoc = new List<Models.Document>();
             listDoc = JsonConvert.DeserializeObject<List<Models.Document>>(JsonConvert.SerializeObject(dt));
+            int totalRow = docDAO.RowCountDocument();
+            int totalPage = (int)Math.Ceiling((decimal)totalRow / 5);
+            ViewBag.TotalPage = totalPage;
+            ViewBag.PageActive = pageNumber;
+            ViewBag.Delete = TempData["delete"];
             return View(listDoc);
-
         }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Models.Document doc)
+        {
+            return View();
+        }
+
+        public IActionResult Update(string id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Delete(string id, int pageNumber)
+        {
+            int result = docDAO.DeleteDoc(id);
+            //if(result > 0)
+            //{
+            //    TempData["delete"] = "true";
+            //    return RedirectToAction("DocumentList", pageNumber);
+            //}
+            //else
+            //{
+            //    TempData["delete"] = "false";
+            //    return RedirectToAction("DocumentList", pageNumber);
+            //}    
+
+            if (result > 0)
+            {
+                return Json(new { success = true });
+            }
+            else
+            {
+                return Json(new { success = false });
+            }
+        }  
     }
 }
--- DocumentSharing/DocumentSharing/DAL/DocumentDAO.cs
+++ DocumentSharing/DocumentSharing/DAL/DocumentDAO.cs
@@ -6,23 +6,67 @@ namespace DocumentSharing.DAL
 {
     public class DocumentDAO
     {
-        public DataTable GetDocument()
+        public DataTable GetDocument(int row)
         {
             ConnectDB.ConnectDatabase();
             SqlConnection conn = ConnectDB.conMyConnection;
-            string query = @"SELECT 
-                                DocId AS DocId, 
-                                DocName AS DocName, 
-                                DocCategory AS DocCategory, 
-                                DocUrl AS DocUrl, 
-                                DocStatus AS DocStatus, 
-                                UserId AS UserId, 
-                                SubjectId AS SubjectId 
-                                FROM Document";
+            string query = $@"DECLARE @pageIndex INT = {row};
+                                DECLARE @rowPage INT = 5;
+                                DECLARE @start INT = @pageIndex;
+                                DECLARE @end INT = @rowPage;
+                                IF @pageIndex > 1
+                                begin
+	                                set @start = (@pageIndex - 1) * @rowPage + 1;
+	                                set @end = @rowPage * @pageIndex;
+                                end
+                                SELECT *
+                                FROM (
+                                    SELECT 
+		                            ROW_NUMBER() OVER (ORDER BY DocId) AS RowNum,
+		                            DocId, 
+                                    DocName, 
+                                    DocCategory, 
+                                    DocUrl, 
+                                    DocStatus, 
+                                    UserId, 
+                                    SubjectId
+                                    FROM Document
+                                ) AS _row
+                                WHERE _row.RowNum BETWEEN @start AND @end";  
+                                //Gọi Stored: EXEC sp_GetDataPaging {row}, 5
             SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, conn);
             DataTable dt = new DataTable();
             sqlDataAdapter.Fill(dt);
             return dt;
+        }
+
+        public int RowCountDocument()
+        {
+            ConnectDB.ConnectDatabase();
+            SqlConnection conn = ConnectDB.conMyConnection;
+            string query = @"SELECT COUNT(1) FROM Document";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            int rowCount = (int)cmd.ExecuteScalar();
+            return rowCount;
+        }
+
+        public int DeleteDoc(string id)
+        {
+            int result = 0;
+            try
+            {
+                ConnectDB.ConnectDatabase();
+                SqlConnection conn = ConnectDB.conMyConnection;
+                string query = @"DELETE FROM Document WHERE DocId = @DocId";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@DocId", id);
+                result = cmd.ExecuteNonQuery();
+            }
+            catch
+            {
+
+            }
+            return result;
         }
     }
 }
